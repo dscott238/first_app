10034f39b472590d1c784c387c5cfbeedc0eb6f6
root <root@localhost.localdomain>
2014-04-10 15:01:45 -0400

    new test

---
+++ deantest/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Deantest::Application.config.secret_key_base = '92e8aec3b59abd0ad36a912a7414761d841f77fcc1af92a3cbffdc2d63cf695e8b831b4a929f1b0851d53cc95083e9fed4c29c8f8739d2eee5c51a330696dc1d'
